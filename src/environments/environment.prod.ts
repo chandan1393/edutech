107b8543bf5638de44518b2565c94ffea7cb3bf2
chandan1393 <chandansharma1393@gmail.com>
2026-05-08 10:22:02 +0530

    change url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Set this to your Railway backend URL after deployment
-  apiUrl: 'https://backend-production-8127.up.railway.app/api'
+  apiUrl: 'https://backend-production-b4ce.up.railway.app/api'
 };
