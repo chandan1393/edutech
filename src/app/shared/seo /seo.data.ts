95fea1e1a56e7d2a93d80e9fbf711b0b9f27f54e
chandan1393 <69247809+chandan1393@users.noreply.github.com>
2026-07-25 19:55:34 +0530

    Create SEO data

---
+++ src/app/shared/seo /seo.data.ts
@@ -0,0 +1,29 @@
+import { SeoModel } from '../models/seo.model';
+
+export const HOME_SEO: SeoModel = {
+  title: 'Online Class Help | Pay Someone to Do My Online Class | EduPilotHelp',
+
+  description:
+    'Affordable online class help from trusted experts. Guaranteed A/B grades. 100% confidential. 24/7 support.',
+
+  canonical: 'https://www.edupilothelp.com/',
+
+  keywords:
+    'online class help, pay someone to do my online class, online course help, homework help, assignment help',
+
+  robots: 'index, follow',
+
+  image: 'https://www.edupilothelp.com/assets/images/og-image.jpg',
+
+  imageAlt: 'EduPilotHelp',
+
+  type: 'website',
+
+  author: 'EduPilotHelp',
+
+  publishedTime: '2026-01-01',
+
+  modifiedTime: '2026-07-25',
+
+  twitterCard: 'summary_large_image'
+};
